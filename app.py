5fc4ee6cc2b4e3e2ac080a5aa9b0c5e8d2a5fe4a
alvarogoncalves04 <alvaro.goncalves0402@gmail.com>
2025-10-16 23:27:30 +0000

    prueba de modulo dash

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import streamlit as st
 
 df = pd.read_csv('pitcheo.csv', encoding = 'latin-1', sep = ';')
 
